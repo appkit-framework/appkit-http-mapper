40571919918198250c0c4fca09d045cda236eecc
appkit-framework <admin@appkit.biz>
2026-03-31 17:59:15 +0200

    Add debug logging

--- src/Resource/Mapper/MapperResource.php
+++ src/Resource/Mapper/MapperResource.php
@@ -26,15 +26,33 @@ class MapperResource extends AbstractHttpResource {
     }
 
     protected function handleRequest($request) {
-        [$matched, $resource, $path] = $this -> mapper -> matchRequest($request);
-        if(!$matched)
+        [$match, $resource, $path] = $this -> mapper -> matchRequest($request);
+
+        $this -> log -> debug(
+            'Matched request',
+            [
+                'requestPath' => $request -> getPath(),
+                'match' => $match,
+                'resource' => $resource !== null ? get_class($resource) : null,
+                'path' => $path
+            ]
+        );
+
+        if(!$match)
             throw new ServerHttpError(404);
 
         if($path == '/')
             return $resource -> dispatchRequest($request);
 
-        $request -> rewritePath(
-            substr($request -> getPath(), strlen($path))
+        $requestPath = $request -> getPath();
+        $rewritePath = substr($requestPath, strlen($path));
+        $request -> rewritePath($rewritePath);
+        $this -> log -> debug(
+            'Rewritten request path {requestPath} to {rewritePath}',
+            [
+                'requestPath' => $requestPath,
+                'rewritePath' => $rewritePath
+            ]
         );
 
         try {
@@ -45,8 +63,18 @@ class MapperResource extends AbstractHttpResource {
             $location = $e -> getLocation();
 
             if(str_starts_with($location, '/')) {
+                $rewriteLocation = $path . $location;
+
+                $this -> log -> debug(
+                    'Rewritten redirect location {location} to {rewriteLocation}',
+                    [
+                        'location' => $location,
+                        'rewriteLocation' => $rewriteLocation
+                    ]
+                );
+
                 throw new ServerHttpRedirect(
-                    $path . $location,
+                    $rewriteLocation,
                     $e -> getResponse() -> getStatus(),
                     previous: $e
                 );
